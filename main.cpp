3d0ca75b46ef10a3055b95d5172a6e61fd48179c
Raghu <k.raghu9926@gmail.com>
2024-12-13 17:38:59 +0530

    Implemented Rubiks Cube 1D Array Representation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "model/RubiksCube3DArrayRepresentation.cpp"
+#include "model/RubiksCube1DArrayRepresentation.cpp"
 using namespace std;
 int main() {
 
@@ -9,15 +10,32 @@ int main() {
     /*
      * Testing RubiksCube3DModel
      */
-    RubiksCube3DArray rubiksCube3DArray;
+    // RubiksCube3DArray rubiksCube3DArray;
+    //
+    // rubiksCube3DArray.print();
+    // if (rubiksCube3DArray.isSolved()) cout << "Cube is Solved!" << endl;
+    // else cout << "Cube is Not Solved!" << endl;
+    //
+    // rubiksCube3DArray.l();
+    // rubiksCube3DArray.print();
+    // if (rubiksCube3DArray.isSolved()) cout << "Cube is Solved!" << endl;
+    // else cout << "Cube is Not Solved!" << endl;
 
-    rubiksCube3DArray.print();
-    if (rubiksCube3DArray.isSolved()) cout << "Cube is Solved!" << endl;
+
+
+    /*
+     * Testing RubiksCube1DModel
+     */
+    RubiksCube1DArray cube;
+
+    cube.print();
+    if (cube.isSolved()) cout << "Cube is Solved!" << endl;
     else cout << "Cube is Not Solved!" << endl;
 
-    rubiksCube3DArray.d();
-    rubiksCube3DArray.print();
-    if (rubiksCube3DArray.isSolved()) cout << "Cube is Solved!" << endl;
+    cube.b();
+    cube.print();
+
+    if (cube.isSolved()) cout << "Cube is Solved!" << endl;
     else cout << "Cube is Not Solved!" << endl;
 
 
